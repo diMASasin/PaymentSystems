5aae6943f1e349dc4012b9f5a922562ec0ef9287
diMASasin <35368058+diMASasin@users.noreply.github.com>
2024-07-20 12:12:27 +0500

    Add argument null check

--- PaymentSystems/PaymentSystem.cs
+++ PaymentSystems/PaymentSystem.cs
@@ -6,20 +6,23 @@ public class PaymentSystem
     private readonly Func<Order, string> _getStringToHash;
     private readonly Func<Order, string, string> _getLink;
 
-    public PaymentSystem(StringHashComputer stringHashComputer, Func<Order, string> stringToHash, 
+    public PaymentSystem(StringHashComputer stringHashComputer, Func<Order, string> stringToHash,
         Func<Order, string, string> linkSignature)
     {
-        _getLink = linkSignature;
-        _getStringToHash = stringToHash;
-        _stringHashComputer = stringHashComputer;
+        _getLink = linkSignature ?? throw new ArgumentNullException(nameof(linkSignature));
+        _getStringToHash = stringToHash ?? throw new ArgumentNullException(nameof(stringToHash));
+        _stringHashComputer = stringHashComputer ?? throw new ArgumentNullException(nameof(stringHashComputer));
     }
-    
+
     public string GetPayingLink(Order order)
     {
+        if (order == null)
+            throw new ArgumentNullException(nameof(order));
+        
         string toHash = _getStringToHash(order);
         string hash = _stringHashComputer.HashString(toHash);
         string link = _getLink(order, hash);
-        
+
         return link;
     }
 }
--- PaymentSystems/StringHashComputer.cs
+++ PaymentSystems/StringHashComputer.cs
@@ -9,11 +9,14 @@ public class StringHashComputer
 
     public StringHashComputer(HashAlgorithm hashAlgorithm)
     {
-        _hashAlgorithm = hashAlgorithm;
+        _hashAlgorithm = hashAlgorithm ?? throw new ArgumentNullException(nameof(hashAlgorithm));
     }
     
     public string HashString(string toHash)
     {
+        if (string.IsNullOrEmpty(toHash) == true) 
+            throw new ArgumentNullException(nameof(toHash));
+        
         byte[] toHashBytes = ASCII.GetBytes(toHash);
         byte[] hash = _hashAlgorithm.ComputeHash(toHashBytes);
         string hashString = BitConverter.ToString(hash).Replace("-", "").ToLower();
